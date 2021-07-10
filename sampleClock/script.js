7ce122c3f37a571e38d583f1d42b210282e72b0f
berkf02 <63246331+berkf02@users.noreply.github.com>
2021-07-10 23:10:38 +0500

    clock update

--- sampleClock/script.js
+++ sampleClock/script.js
@@ -7,9 +7,20 @@ setInterval(()=> {
     let hours = new Date().getHours();
     let minutes = new Date().getMinutes();
     let seconds = new Date().getSeconds();
+
+    if (hours < 10) {
+        hours = '0' + hours
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds
+    }
+
     hourSpan.textContent = hours.toString();
-    minutesSpan.textContent = minutes.toString()
-    secondsSpan.textContent = seconds.toString()
+    minutesSpan.textContent = minutes.toString();
+    secondsSpan.textContent = seconds;
     console.log(`${hours}: ${minutes}: ${seconds}`)
 }, 1000)
 
